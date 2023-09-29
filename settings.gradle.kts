1f127534d6ecd7f32cce5b5506572d2c05bd6023
korilin <korilin.dev@gmail.com>
2023-09-29 23:05:54 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
-rootProject.name = "kotpot"
+rootProject.name = "gerbera"
 
 include(":gerbera-common")
 include(":gerbera-commands")
